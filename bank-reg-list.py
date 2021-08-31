c7d8db139bb7c10a2d8bb84fd9e516e2feacf70f
s06akin <sobakin.sun@ya.ru>
2021-08-31 23:18:47 +0300

    change interval schedule

--- bank-reg-list.py
+++ bank-reg-list.py
@@ -5,7 +5,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', minute=5)
+@sched.scheduled_job('interval', minutes=3)
 def scheduled_job():
     data = []
     result = []
