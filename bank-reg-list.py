7e21d2c140131e59f42eacd9fa74c41a3277c4f5
s06akin <sobakin.sun@ya.ru>
2021-09-01 13:01:19 +0300

    change hour schedule

--- bank-reg-list.py
+++ bank-reg-list.py
@@ -5,7 +5,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=10)
+@sched.scheduled_job('cron', hour=7)
 def scheduled_job():
     data = []
     result = []
