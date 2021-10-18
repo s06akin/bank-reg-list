2d70ee22c2ec71f69e65233f36aa943855e4d668
s06akin <sobakin.sun@ya.ru>
2021-10-18 12:16:10 +0300

    added moratoriy parse

--- bank-reg-list.py
+++ bank-reg-list.py
@@ -11,27 +11,39 @@ def scheduled_job():
     result = []
     reg = []
     
-    url = 'https://cbr.ru/banking_sector/likvidbase/PartSelectorState1//'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    find_table = soup.find('table', class_='data')
-    
-    rows = find_table.find_all('tr')
-    
-    for row in rows:
+    otzyv_url = 'https://cbr.ru/banking_sector/likvidbase/PartSelectorState1//'
+    otzyv_response = requests.get(otzyv_url)
+    otzyv_soup = BeautifulSoup(otzyv_response.text, 'lxml')
+    otzyv_find_table = otzyv_soup.find('table', class_='data')
+    otzyv_rows = otzyv_find_table.find_all('tr')
+
+    for row in otzyv_rows:
         cols = row.find_all('td')
         cols = [el.text.strip() for el in cols]
         if cols:
             data.append([el for el in cols if el])
-    
+
     for i in data:
         for j in i: 
             result.append(j)
-        
     
     for index in range(len(result)):
         if index % 2 == 0:
             reg.append(result[index])
+        
+
+    moratoriy_url = 'https://cbr.ru/banking_sector/PrBankrot/moratoriy/'
+    moratoriy_response = requests.get(moratoriy_url)
+    moratoriy_soup = BeautifulSoup(moratoriy_response.text, 'lxml')
+    moratoriy_find_dropdown = moratoriy_soup.find('div', id='DropDown_content')
+    moratoriy_find_table = moratoriy_find_dropdown.find('table')
+    moratoriy_find_reg = moratoriy_find_table.find_all('td', width='60px')
+
+    moratoriy_reg = [el.text.strip() for el in moratoriy_find_reg] 
+
+    if len(moratoriy_reg) != 0:
+        for index in range(len(moratoriy_reg)):
+            reg.append(moratoriy_reg[index])
         
     
     smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
